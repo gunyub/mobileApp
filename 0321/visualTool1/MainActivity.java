d0dd42732002d5c3d608285fe5eb37892752134a
gunyub <gunyub1120@gmail.com>
2025-03-27 23:12:55 +0900

    Add files via upload

---
+++ 0321/visualTool1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.visualtool1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import android.net.Uri;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-6414-2103"));
+        startActivity(intent);
+    }
+    }
+
