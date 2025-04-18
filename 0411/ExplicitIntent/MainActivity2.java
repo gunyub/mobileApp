1be85b3e7ac29cf6a866e680f44b1b15aecb953e
gunyub <gunyub1120@gmail.com>
2025-04-18 13:37:39 +0900

    Add files via upload

---
+++ 0411/ExplicitIntent/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.togglebuttontest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button b = (Button)findViewById(R.id.Button01);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                finish();
+            }
+        });
+    }
+}
